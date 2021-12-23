d77c37c47b89952d76d26f07471f216ae55d48d9
himnish17 <81815516+himnish17@users.noreply.github.com>
2021-12-23 08:53:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+status="";
+
+function setup() {
+    canvas = createCanvas(380 , 380);
+    canvas.center();
+    video = createCaptue(VIDEO);
+    video.size(380 , 380);
+    video.hide();
+}
+
+function draw() {
+    image(video , 0 , 0 , 380 , 380);
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("ModelLoaded!");
+    status=true;
+}
